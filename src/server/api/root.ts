3c2dc0f8f2b144227c0be95c6f29f6d02af145dc
Petros Kitazos <petroskitazos@gmail.com>
2023-04-30 22:22:50 +0100

    created templateRouter and first router procedure to fetch data from the database to the client

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { templateRouter } from "~/server/api/routers/templateRouter";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  templateRouter,
 });
 
 // export type definition of API
---
+++ src/server/api/routers/templateRouter.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const templateRouter = createTRPCRouter({
+  getTemplateAverage: publicProcedure
+    .input(z.string())
+    .query(async ({ ctx, input: templateID }) => {
+      let fields = await ctx.prisma.templateField.findMany({
+        where: {
+          graphTemplateID: templateID,
+        },
+      });
+
+      return fields.reduce(async (acc, val) => {
+        return {
+          ...acc,
+          [val.name]:
+            (
+              await ctx.prisma.fieldValue.aggregate({
+                _avg: {
+                  value: true,
+                },
+                where: {
+                  fieldID: val.ID,
+                },
+              })
+            )._avg.value || 5,
+        };
+      }, {}) as Record<string, number>;
+    }),
+});
