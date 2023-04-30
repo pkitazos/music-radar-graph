92b3a3ffcb994d162d2931ebc3fb4389c145bc7f
Petros Kitazos <petroskitazos@gmail.com>
2023-04-30 03:51:32 +0100

    added new Icons

---
+++ src/SVGs/DiscIcon.tsx
@@ -0,0 +1,16 @@
+const DiscIcon = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <circle cx="12" cy="12" r="10"></circle>
+        <circle cx="12" cy="12" r="3"></circle>
+      </svg>
+    </>
+  );
+};
+
+export default DiscIcon;
