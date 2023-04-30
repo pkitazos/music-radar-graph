6152a393566dd58a53c05659cb51ecc5549d7504
Petros Kitazos <petroskitazos@gmail.com>
2023-04-30 03:20:09 +0100

    updated side menu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,57 +1,41 @@
 import Link from "next/link";
-import {
-  GraphIcon,
-  SaveIcon,
-  HomeIcon,
-  InfoIcon,
-  PlusIcon,
-  RadarIcon,
-} from "~/components/SVGs";
-import { defaultChart } from "~/data";
+import { ReactNode } from "react";
+import { HomeIcon, InfoIcon } from "~/SVGs";
+import PieChartIcon from "~/SVGs/PieChartIcon";
 
 interface props {
-  selected: number;
-  savedCharts: chartData[];
-  setSavedCharts: React.Dispatch<React.SetStateAction<chartData[]>>;
-  setSelectedChart: React.Dispatch<React.SetStateAction<number>>;
+  children?: ReactNode[];
 }
 
-const SideMenu = ({
-  selected,
-  savedCharts,
-  setSavedCharts,
-  setSelectedChart,
-}: props) => {
-  const colorFill = (i: number) => {
-    if (!savedCharts[i]?.albumName) return "text-warning";
-    if (selected === i) return "text-lime-500";
-    return "";
-  };
+const SideMenu = ({ children }: props) => {
+  // const colorFill = (i: number) => {
+  //   if (!savedCharts[i]?.albumName) return "text-warning";
+  //   if (selected === i) return "text-lime-500";
+  //   return "";
+  // };
   return (
     <div className="left-0 top-0 h-screen">
       <ul className="menu rounded-box w-max gap-1 p-2">
+        <li className="tooltip tooltip-right" data-tip="Info">
+          <div className="text-yellow-500 hover:bg-slate-800/50">
+            <InfoIcon />
+          </div>
+        </li>
         <li className="tooltip tooltip-right" data-tip="Home Page">
           <Link href="/" className="hover:bg-slate-800/50">
             <HomeIcon />
           </Link>
         </li>
+
         <li className="tooltip tooltip-right" data-tip="Graph Page">
-          <Link href="/global-graphs" className="hover:bg-slate-800/50">
-            <RadarIcon />
+          <Link href="/graphs" className="hover:bg-slate-800/50">
+            <PieChartIcon />
           </Link>
         </li>
         <div className="divider" />
 
-        <li>
-          <div className="hover:bg-slate-800/50">
-            <InfoIcon />
-          </div>
-        </li>
-        <li>
-          <div className="hover:bg-slate-800/50">
-            <SaveIcon />
-          </div>
-        </li>
+        {/* //* adds new chart -> move to graph creation part of app */}
+        {/*
         <li className="tooltip tooltip-right" data-tip="create new Graph">
           <div
             className="hover:bg-slate-800/50"
@@ -62,9 +46,9 @@ const SideMenu = ({
           >
             <PlusIcon />
           </div>
-        </li>
-        <div className="divider" />
-        {savedCharts &&
+        </li> */}
+        {/* //* Something about saved charts (will load using custom hook) */}
+        {/* {savedCharts &&
           savedCharts.map((_, i) => (
             <li
               key={i}
@@ -78,7 +62,13 @@ const SideMenu = ({
                 <GraphIcon />
               </button>
             </li>
-          ))}
+          ))} */}
+        {children?.length && (
+          <>
+            {children}
+            <div className="divider" />
+          </>
+        )}
       </ul>
     </div>
   );
