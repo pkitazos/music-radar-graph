921d503f121ad3d50d9e4dda4ae8a09786877dea
Petros Kitazos <petroskitazos@gmail.com>
2023-05-01 12:28:58 +0100

    updated pageInfo with correct colors

--- src/pages/data/pageInfo.ts
+++ src/pages/data/pageInfo.ts
@@ -3,20 +3,20 @@ const pageInfo = {
     title: "A Moon Shaped Pool",
     color: { H: 0, S: 0, L: 100 },
   },
-  "kid-a": { title: "Kid A", color: { H: 0, S: 0, L: 100 } },
-  amnesiac: { title: "Amnesiac", color: { H: 0, S: 0, L: 100 } },
-  "ok-computer": { title: "OK Computer", color: { H: 0, S: 0, L: 100 } },
+  "kid-a": { title: "Kid A", color: { H: 354, S: 93, L: 22 } },
+  amnesiac: { title: "Amnesiac", color: { H: 0, S: 100, L: 41 } },
+  "ok-computer": { title: "OK Computer", color: { H: 217, S: 100, L: 61 } },
   "hail-to-the-thief": {
     title: "Hail to the Thief",
-    color: { H: 0, S: 0, L: 100 },
+    color: { H: 23, S: 97, L: 46 },
   },
-  "in-rainbows": { title: "In Rainbows", color: { H: 0, S: 0, L: 100 } },
+  "in-rainbows": { title: "In Rainbows", color: { H: 20, S: 100, L: 50 } },
   "the-king-of-limbs": {
     title: "The King of Limbs",
-    color: { H: 0, S: 0, L: 100 },
+    color: { H: 82, S: 77, L: 40 },
   },
-  "the-bends": { title: "The Bends", color: { H: 0, S: 0, L: 100 } },
-  "pablo-honey": { title: "Pablo Honey", color: { H: 0, S: 0, L: 100 } },
+  "the-bends": { title: "The Bends", color: { H: 359, S: 94, L: 62 } },
+  "pablo-honey": { title: "Pablo Honey", color: { H: 46, S: 100, L: 50 } },
 };
 
 export default pageInfo;
