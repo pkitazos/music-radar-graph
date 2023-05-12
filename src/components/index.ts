c1de5a5f7ef9243b09c182c7ac08cd76670256dc
Petros Kitazos <petroskitazos@gmail.com>
2023-05-13 00:39:56 +0100

    removed chart.js  and react-chartjs-2 libraries

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { default as AlbumButton } from "./AlbumButton";
 export { default as AlbumModal } from "./AlbumModal";
 export { default as GraphMaker } from "./GraphMaker";
 export { default as PageButton } from "./PageButton";
-export { default as RadarChart } from "./RadarChart";
 export { default as RadarGraph } from "./RadarGraph";
 export { default as RangeSlider } from "./RangeSlider";
 export { default as SideMenu } from "./SideMenu";
