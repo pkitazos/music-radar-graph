cb2e327a690b7d529878684dab5f9686d836caa4
pkitazos <petroskitazos@gmail.com>
2023-05-25 10:15:05 +0100

    added basic spinner to Loading component

--- src/components/base/Loading.tsx
+++ src/components/base/Loading.tsx
@@ -1,9 +1,13 @@
-import { FC } from "react";
-
 const Loading = () => {
   return (
     <>
-      <div>loading . . .</div>
+      <div
+        className="inline-block h-8 w-8 animate-spin rounded-full border-[3px] border-current border-t-transparent text-secondary"
+        role="status"
+        aria-label="loading"
+      >
+        <span className="sr-only">Loading...</span>
+      </div>
     </>
   );
 };
