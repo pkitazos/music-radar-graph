96fa43d1327f087c1ebaf7c4a2c2ab81bfe5e9ce
pkitazos <petroskitazos@gmail.com>
2023-07-01 13:28:38 +0100

    fixed type imports

--- src/pages/graphs/[album]/index.tsx
+++ src/pages/graphs/[album]/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { GetStaticPaths, GetStaticProps, type NextPage } from "next";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
