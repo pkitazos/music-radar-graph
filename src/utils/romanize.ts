36bf0bc585fdbf0f80523c37ffb8f5e826f9f21c
pkitazos <petroskitazos@gmail.com>
2023-07-01 13:28:21 +0100

    fixed typing

--- src/utils/romanize.ts
+++ src/utils/romanize.ts
@@ -1,24 +1,22 @@
 const romanize = (num: number) => {
-  // TODO: sort out i type
-
   const lookup = {
-      M: 1000,
-      CM: 900,
-      D: 500,
-      CD: 400,
-      C: 100,
-      XC: 90,
-      L: 50,
-      XL: 40,
-      X: 10,
-      IX: 9,
-      V: 5,
-      IV: 4,
-      I: 1,
-    },
-    roman = "",
-    i;
-  for (i in lookup) {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let roman = "";
+
+  for (const i in lookup) {
     while (num >= lookup[i as keyof typeof lookup]) {
       roman += i;
       num -= lookup[i as keyof typeof lookup];
